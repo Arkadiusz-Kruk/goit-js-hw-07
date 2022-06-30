8b7a786956bd7d2d463477f29e9e9cf398d06d29
Arkadiusz-Kruk <92742646+Arkadiusz-Kruk@users.noreply.github.com>
2022-06-30 12:03:47 +0200

    updt gallery01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map((item) => `<div class="gallery__item">
+                    <a class="gallery__link" href="${item.preview}">
+                      <img
+                        class="gallery__image"
+                        src="${item.preview}"
+                        data-source="${item.original}"
+                        alt="${item.description}"
+                      />
+                    </a>
+                  </div>`
+  )
+  .join("");
+    
+gallery.insertAdjacentHTML("afterbegin", markup);
+gallery.addEventListener('click', openModal);
+
+
+function openModal(event) { 
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  event.preventDefault(); 
+  const imageLink = event.target.dataset.source;
+  const instance = basicLightbox.create(
+    `<img src="${imageLink}">`,
+    {
+      onShow: (instance) => {
+        document.addEventListener("keydown", e => {
+          if (e.code === "Escape") {
+            instance.close();
+          }
+        });
+        
+      }
+    }  
+    );
+  instance.show();
+}  
